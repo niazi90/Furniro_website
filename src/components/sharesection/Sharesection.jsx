9aa5d0dbcedd0bfa54720c4f95498ef3da7a8347
Azhar Iqbal <niazi7589@gmail.com>
2025-12-15 16:00:49 +0500

    grid update

--- src/components/sharesection/Sharesection.jsx
+++ src/components/sharesection/Sharesection.jsx
@@ -9,7 +9,19 @@ const ShareSection = () => {
         <span>#FuniroFurniture</span>
       </h2>
 
-      <div className="share-grid">
+
+
+<div class="parent">
+    <div class="div1"> <img src="/1.png" alt="" /></div>
+    <div class="div3"><img src="/2.png"  alt="" /></div>
+    <div class="div4"> <img src="/5.png"  alt="" /></div>
+    <div class="div5"><img src="/6.png"  alt="" /></div>
+    <div class="div6"> <img src="/Rectangle40.png"  alt="" /></div>
+    <div class="div7"> <img src="/4.png"  alt="" /></div>
+    <div class="div8">  <img src="/8.png"  alt="" /></div>
+    <div class="div9"> <img src="/7.png"  alt="" /></div>
+</div>
+      {/* <div className="share-grid">
 
         <img src="/1.png" className="item item1" alt="" />
         <img src="/2.png" className="item item2" alt="" />
@@ -21,7 +33,7 @@ const ShareSection = () => {
 
         <img src="/8.png" className="item item7" alt="" />
         <img src="/7.png" className="item item8" alt="" />
-      </div>
+      </div> */}
     </section>
   );
 };
